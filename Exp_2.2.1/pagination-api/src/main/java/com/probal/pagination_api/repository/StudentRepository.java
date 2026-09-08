68b9379537def0021fa2412d18c2345091be7d14
Probal Dhali <probaldhali2006@gmail.com>
2026-09-08 22:18:13 +0530

    Delete Exp_2.2.1/pagination-api directory

--- Exp_2.2.1/pagination-api/src/main/java/com/probal/pagination_api/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.probal.pagination_api.repository;
-
-import com.probal.pagination_api.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
